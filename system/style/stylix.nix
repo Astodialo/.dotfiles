66773cd59400c0ea2de65075d2daea644b60106e
Astodialo <panteliskavv@gmail.com>
2024-09-05 23:45:54 +0200

    stylix and kitty config

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, inputs, ...}:
 
 {
-  stylix.enable = true
+  stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-soft.yaml";
   stylix.image = ./wallpapers/pink-trees.jpeg;
 }
